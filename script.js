85c6953c1207461b041583f92c750cf28c9ea37b
Ramaiya26 <ramaiyakumar26@gmail.com>
2023-08-20 22:36:20 +0530

    First commit of my site

---
+++ script.js
@@ -0,0 +1,8 @@
+const formOpenBtn = document.querySelector("#form-open"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
+formCloseBtn = document.querySelector(".form_close"),
+signupBtn = document.querySelector("#signup"),
+loginBtn = document.querySelector("#login"),
+pwShowHide = document.querySelector(".pw_hide");
+
