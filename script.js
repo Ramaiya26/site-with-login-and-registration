f86db3e138d3bbbd13676a08b1febc664701fa5d
Ramaiya26 <ramaiyakumar26@gmail.com>
2023-08-21 15:56:28 +0530

    second commit

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ signupBtn = document.querySelector("#signup"),
 loginBtn = document.querySelector("#login"),
 pwShowHide = document.querySelector(".pw_hide");
 
+formOpenBtn.addEventListener("click",()=> home.classList.add("show"));
+formCloseBtn.addEventListener("click",()=> home.classList.remove("show"));
+
+// pwShowHide.forEach((icon) => {
+//     icon.addEventListener("click",() => {
+//         let getPwInput = icon.parentElement.querySelector("input");
+//         if (getPwInput.type === "password"){
+//             getPwInput.type = "text";
+//             icon.classList.replace("uil-eye-slash", "uil-eye");
+
+//         }else{
+//             getPwInput.type = "passsword";
+//             icon.classList.replace("uil-eye", "uil-eye-slash");
+//         }
+//     });
+// });
+signupBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.add("active");
+})
+loginBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    formContainer.classList.remove("active");
+})
+
